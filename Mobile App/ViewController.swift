715b5660baed2c6213e57bc9db825eab9572a078
Shivani Reddy <singireddyshivanireddy@gmail.com>
2019-01-31 13:36:43 -0800

    Pushed the product changes

--- Mobile App/ViewController.swift
+++ Mobile App/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var BillLabel: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalBill: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+       
+        view.endEditing(true)
+    }
+    
+    @IBAction func CalucaleTip(_ sender: Any) {
+        //fetching the bill
+        let bill = Double(BillLabel.text!) ?? 0
+         let tipPercents = [0.1,0.15,0.2]
+        let tip = bill*tipPercents[tipControl.selectedSegmentIndex]
+        let totalAmount = bill + tip
+       
+        //Finally Updating values
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalBill.text = String(format: "$%.2f", totalAmount)
+    }
+    
 }
 
